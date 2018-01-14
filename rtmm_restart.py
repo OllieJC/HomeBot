8f5b54ea2558f1338b200f6e8a5d1515e68d418f
Pr09het <oliver.chalk@outlook.com>
2018-01-14 22:15:03 +0000

    Restart function

--- rtmm_restart.py
+++ rtmm_restart.py
@@ -2,18 +2,23 @@ def enabled():
     return True
 
 def initial_run(input_text):
-    return False
-
-def run(input_text):
     if input_text == "restart" or input_text == "reboot":
         return "Must be root."
     elif input_text == "sudo restart" or input_text == "sudo reboot":
         return "Restarting..."
+
+def run(input_text):
+    import subprocess, sys
+    if input_text == "sudo restart" or input_text == "sudo reboot":
+        if sys.platform == "win32":
+            subprocess.call(["shutdown", "-r", "/t", "0"])
+        else:
+            subprocess.call(["sudo", "reboot"])
 
 def commands():
     if not enabled():
         return False
 
     return [
-        {"command": ["restart", "reboot", "sudo restart", "sudo reboot"], "text": "restarts HomeBot host server/computer", "args": "0"}
+        {"command": ["restart", "reboot", "sudo restart", "sudo reboot"], "text": "restarts HomeBot host server/comp$
     ]
